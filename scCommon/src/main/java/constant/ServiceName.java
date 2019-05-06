bfa4ae5665292684253ede8ce663bad6a22484d1
DavenChan <396767101@qq.com>
2019-05-06 22:14:52 +0800

    init

---
+++ scCommon/src/main/java/constant/ServiceName.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class ServiceName {
+
+	public static final String serviceHi = "service-hi";
+}
---
+++ scCommon/src/main/java/constant/Urls.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class Urls {
+	
+	public static final String hi = "/hi";
+
+}
