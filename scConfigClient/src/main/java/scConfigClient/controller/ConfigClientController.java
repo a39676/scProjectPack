67c6774472e8de092da9d1e7d2b3656ec090f970
DavenChan <396767101@qq.com>
2019-05-09 14:03:43 +0800

    实时刷新 configuration , 配置可能有误

--- scConfigClient/src/main/java/scConfigClient/controller/ConfigClientController.java
+++ scConfigClient/src/main/java/scConfigClient/controller/ConfigClientController.java
@@ -1,12 +1,14 @@
 package scConfigClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import constant.Urls;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
 	@Value("${neo.hello}")
