79a8c4821288eed96c8f3075dfcf3fd585219721
DavenChan <396767101@qq.com>
2019-07-16 19:04:20 +0800

    image project 补充;部分公共文件转移位置;删除冗余文件;

---
+++ imageProject/src/main/java/imageProject/ImagesApplication.java
@@ -0,0 +1,20 @@
+package imageProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan(basePackages = {"scAppDemo.base", "scAppDemo.config"})
+@ComponentScan(basePackages = {"scAppDemo.test"})
+@EnableDiscoveryClient
+public class ImagesApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run( ImagesApplication.class, args );
+    }
+
+}
